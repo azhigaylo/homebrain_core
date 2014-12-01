8898449b2b9e2bc511f5413fb4c0786e46ddc2b7
AZhigaylo <antoooon@gmail.com>
2014-12-01 17:59:34 +0200

    work

--- pal/os_wraper/CxSysTimer.h
+++ pal/os_wraper/CxSysTimer.h
@@ -1,104 +1,51 @@
 #ifndef _CX_SYSTIMER
 #define _CX_SYSTIMER
 
-
 //------------------------------------------------------------------------------
-// DO NOT USE FOR FUNCTION WHICH NEEDS LONG PROCESSOR TIME
-// vApplicationTickHook SECTION IS COLLED FROM OS KERNEL 
-// TIME CRITICAL 
-// ONLY FOR INTERVAL TIMER
-//------------------------------------------------------------------------------
+#include <signal.h>
+#include <time.h>
 
-
-//----------------------only wraper for FREERTOS xQueue-------------------------
-
-#include "FreeRTOS.h"
-#include "task.h"
+#include "CxQueue.h"
 
 //------------------------------------------------------------------------------
 
+#define configMAX_TIMER_NAME_LEN 50
 //------------------------------------------------------------------------------
 
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-   void vApplicationTickHook( void );
-#ifdef __cplusplus
-}
-#endif
-
-//------------------------------------------------------------------------------
-//---------------------class for system virtual timer's-------------------------
-//------------------------------------------------------------------------------
-
-class CxSysVirtualTimer;
-typedef class CxSysVirtualTimer *pCxSysVirtualTimer;
-
-struct TListOfVirtualTimers
-{                                                                               // sructure for list 
-    pCxSysVirtualTimer  FirstElem;
-    pCxSysVirtualTimer  LastElem;
-    unsigned char  CounterTimer;
-}; 
-    
-//------------------------------------------------------------------------------
-
-class CxSysVirtualTimer                                                                  // class of virtual timer's
+// name   - timer name, for easy debug only
+// period - in nanosecs
+// cyclic - if it true - timer will generate cyclic event
+  
+class CxSysTimer                                     // class of virtual timer's
 {  
-  public:                    
-    pCxSysVirtualTimer    pNextTmr;                                                       // pointer on next virtual timer
-    
-    enum TMode
-    {
-      cycle = 1,
-      singl
-    };
-    enum TState
-    {
-      on = 1,
-      off
-    };                                                        // timer on/off  in start time  
-
-    CxSysVirtualTimer(void (*execution_func)(), unsigned long period, TMode mode, TState state);
-    ~CxSysVirtualTimer();
-          
-    void  Start();                                         // start timer    
-    void  Stop()   {mCount = 0; mWork = false;}            // stop timer
-    void  Cycle()  {mCycle = cycle;}                       // timer in cicle
-    void  NoCycle(){mCycle = singl;}                       // out fro cikl work timer's
-    bool  Work()   {return(mWork);}                        // while timer count - true  
-    void  Dec();                                           // check on  
-    
-  private:               
-    
-    void AddToList();                                      // add virtual timer in work list
-    void Over();                                           // function of overflow of virtual timer's                        
- 
-    void (*exec_func)();                                   // execution function 
-    unsigned long         mCount;                          // work period
-    TMode                 mCycle;                          // cyclic flag 
-
-  public:  
-    bool                  mWork;                           // work flag 
-    unsigned long         mCountConst;                     // constant timer period
-};
-
-//------------------------------------------------------------------------------
-
-class CxTSysBase
-{
   public:
-    
-    static CxTSysBase& getInstance();    
+   
+    CxSysTimer( const char *name, uint64_t period, bool cyclic );  
+    ~CxSysTimer();
+          
+    void Start();                                               // start timer
+    void Stop();                                                // stop timer
 
-    void SysTic();
-    
-  protected:
-    
-    CxTSysBase();
-    ~CxTSysBase(){}
+  protected:  
 
-}; typedef CxTSysBase *pCxTSysBase;
+    char timerName[configMAX_TIMER_NAME_LEN];    
+
+  private:
+
+    static uint16_t timer_sigID;
+	
+	int64_t expCount;	     // expiration counter
+	timer_t timerid;         // timer ID
+    struct sigevent sev;     // signal structure
+    struct itimerspec its;   // time spec structure
+    struct sigaction sa;     // sigaction structure
+	
+    static void commonHandler(int sig, siginfo_t *si, void *uc);     // start timer
+
+	virtual void sigHandler();
+	
+    CxSysTimer( const CxSysTimer & ){}
+    CxSysTimer & operator=( const CxSysTimer & ){}
+};
 
 #endif
