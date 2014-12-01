1e6e49052cb2982f4641a1cbed2fe06f50ab225d
AZhigaylo <antoooon@gmail.com>
2014-12-01 17:59:30 +0200

    work

--- pal/os_wraper/CxSysTimer.cpp
+++ pal/os_wraper/CxSysTimer.cpp
@@ -1,131 +1,116 @@
+//------------------------------------------------------------------------------
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <signal.h>
+#include <time.h>
+//------------------------------------------------------------------------------
+#include "slog.h"
 #include "utils.h"
-#include "AT91SAM7S128.h"
-#include "at91sam7s128\lib_AT91SAM7S.h"
 #include "CxSysTimer.h"
-
-
-
 //------------------------------------------------------------------------------
-CxTSysBase &tSysBase= CxTSysBase::getInstance();
-//------------------------------------------------------------------------------
-TListOfVirtualTimers ListOfVirtualTimers;  
+uint16_t CxSysTimer::timer_sigID = SIGRTMIN;
 //------------------------------------------------------------------------------
 
-CxSysVirtualTimer::CxSysVirtualTimer(void (*execution_func)(), unsigned long period, TMode mode, TState state):
-   mCountConst  ( period )
-  ,exec_func    ( execution_func ) 
-  ,mWork        ( false )  
-  ,mCycle       ( mode ) 
+CxSysTimer::CxSysTimer( const char *name, uint64_t period, bool cyclic ): 
+    timerid ( 0 )
+   ,expCount( 0 )
 {
-    AddToList();  
-    
-    if( state == CxSysVirtualTimer::on )
-    {
-      Start();
-    }  
-}
-
-void CxSysVirtualTimer::Start()  
-{
-  mCount = mCountConst >> 2; 
-  
-  mWork = true;
-} 
+   memset_m( &sev, 0, sizeof(sev), sizeof(sev) );
+   memset_m( &its, 0, sizeof(its), sizeof(its) );
+   memset_m( &sa,  0, sizeof(sa),  sizeof(sa)  );
    
-CxSysVirtualTimer::~CxSysVirtualTimer()       
-{
-
-}
-
-//------------------------------------------------------------------------------
-
-void  CxSysVirtualTimer::AddToList()
- {
-   pCxSysVirtualTimer CurrentTmr;                                                        // pointer on next virtual timer 
+   strncpy_m( timerName, const_cast<char*>(name), configMAX_TIMER_NAME_LEN );
    
-   CurrentTmr = this;
-   CurrentTmr->pNextTmr = NULL;
-   if( ListOfVirtualTimers.CounterTimer == 0 )
+   // establish handler for timer signal
+   sa.sa_flags = SA_SIGINFO;
+   sa.sa_sigaction = CxSysTimer::commonHandler;
+   sigemptyset(&sa.sa_mask);
+   if (sigaction(timer_sigID, &sa, NULL) == -1)
    {
-     ListOfVirtualTimers.FirstElem =  ListOfVirtualTimers.LastElem = CurrentTmr;
+      printError("CxSysTimer/%s: timer=%s sigaction error!!!", __FUNCTION__, timerName);
+   }
+
+   // create the timer
+   sev.sigev_notify = SIGEV_SIGNAL;
+   sev.sigev_signo = timer_sigID;
+   sev.sigev_value.sival_ptr = (void*) this;
+   if (timer_create(CLOCK_MONOTONIC, &sev, &timerid) == -1)
+   {
+      printError("CxSysTimer/%s: timer=%s timer_create error!!!", __FUNCTION__, timerName);
+   }
+   
+   // init the timer
+   its.it_value.tv_sec = period / 1000000000;
+   its.it_value.tv_nsec = period % 1000000000;
+   its.it_interval.tv_sec = its.it_value.tv_sec;
+   its.it_interval.tv_nsec = its.it_value.tv_nsec;   
+   
+   printDebug("CxSysTimer/%s: timer=%s / ID=0x%lx", __FUNCTION__, timerName, (long) timerid);
+}
+
+void CxSysTimer::Start()
+{
+   if (timerid != 0)
+   {
+      if (timer_settime(timerid, 0, &its, NULL) == -1)
+      {
+         printError("CxSysTimer/%s: timer=%s timer_settime error!!!", __FUNCTION__, timerName);
+      }
    }
    else
    {
-     ListOfVirtualTimers.LastElem->pNextTmr = CurrentTmr;
-     ListOfVirtualTimers.LastElem = CurrentTmr;
+      printError("CxSysTimer/%s: timer=%s timerid = 0 !", __FUNCTION__, timerName);
    }
+} 
+void CxSysTimer::Stop()
+{
+   struct itimerspec its_stop;
+   // set Stop values ( 0 )
+   its_stop.it_value.tv_sec  = 0;
+   its_stop.it_value.tv_nsec = 0;
 
-   ListOfVirtualTimers.CounterTimer++;     
- }
-
-//------------------------------------------------------------------------------
-
-void  CxSysVirtualTimer::Dec()
- {
-   if( mCount != 0)
+   if (timerid != 0)
    {
-     if(--mCount == 0)
-     {
-       Over();
-     }  
-   }
- }
- 
-//------------------------------------------------------------------------------
-
-void  CxSysVirtualTimer::Over()
- {
-   mWork = false;
-   if( NULL != exec_func )(*exec_func)();
-   if( mCycle == CxSysVirtualTimer::cycle ) Start();   
- }
- 
-//------------------------------------------------------------------------------
-//------------------------------------------------------------------------------
-//------------------------------------------------------------------------------
- 
-CxTSysBase::CxTSysBase()
-{
-
-}
-
-CxTSysBase &CxTSysBase::getInstance()
-{
-  static CxTSysBase theInstance;
-  return theInstance;
-}
-
-void CxTSysBase::SysTic()
-{
-  unsigned char iteration=0;
-  pCxSysVirtualTimer CurrentTmr;          // pointer on next virtual timer 
-
-  if( ListOfVirtualTimers.CounterTimer != 0 )
-  {
-    CurrentTmr = ListOfVirtualTimers.FirstElem;    
-    while( CurrentTmr->pNextTmr != NULL )
-    {
-      if( CurrentTmr->mWork )
+      if (timer_settime(timerid, 0, &its, NULL) == -1)
       {
-        CurrentTmr->Dec();
-      }  
-      CurrentTmr = CurrentTmr->pNextTmr;
-      
-      if( iteration > ListOfVirtualTimers.CounterTimer )
-      {
-        return;
+         printError("CxSysTimer/%s: timer=%s timer_settime error!!!", __FUNCTION__, timerName);
       }
-      iteration++;
-     }
-     
-    CurrentTmr->Dec();
-  }  
+   }
+   else
+   {
+      printError("CxSysTimer/%s: timer=%s timerid = 0 !", __FUNCTION__, timerName);
+   }
+   
+   printDebug("CxSysTimer/%s: timer=%s / ID=0x%lx Stopped", __FUNCTION__, timerName, (long) timerid);
+}
+
+CxSysTimer::~CxSysTimer()
+{
+   if (timerid != 0)
+   {
+      timer_delete( timerid );
+	  printDebug("CxSysTimer/%s: timer=%s / ID=0x%lx Deleted", __FUNCTION__, timerName, (long) timerid);
+   }
 }
 
 //------------------------------------------------------------------------------
-// Hook functions
 
-void vApplicationTickHook( void )
-{
-  tSysBase.SysTic(); 
+void CxSysTimer::commonHandler(int sig, siginfo_t *si, void *uc)
+{   
+   CxSysTimer *pSysTimer = (reinterpret_cast<CxSysTimer*>(si->si_value.sival_ptr)) ;
+
+   if (pSysTimer != 0)
+   {
+      pSysTimer->sigHandler();
+	  pSysTimer->expCount++;
+   }
 }
+
+void CxSysTimer::sigHandler()
+{
+   printWarning("CxSysTimer/%s: timer=%s expired n=%i times, handler not implemented!", __FUNCTION__, timerName, expCount);
+}
+
+
+//------------------------------------------------------------------------------
