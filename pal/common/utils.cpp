5e47f3e03ab734bf2879412fcf8c51bde55b1e27
AZhigaylo <antoooon@gmail.com>
2014-12-04 11:08:00 +0200

    work

--- pal/common/utils.cpp
+++ pal/common/utils.cpp
@@ -1,8 +1,22 @@
+//------------------------------------------------------------------------------
 #include <string.h>
 #include <math.h>
 #include <stdarg.h>
+#include <unistd.h>
+#include <iostream>
+//------------------------------------------------------------------------------
 #include "utils.h"
-//#include "CxAssertions.h"
+//-----------------------------System Utilities---------------------------------
+
+void sleep_mcs( uint64_t delay_in_ms)
+{
+   usleep(delay_in_ms);
+}
+
+void sleep_s ( uint64_t delay_in_s )
+{
+   sleep(delay_in_s);
+}
 
 //-----------------------------Utilities-Routines-------------------------------
 
@@ -129,7 +143,7 @@ static const unsigned char CRC_Lo[] = {
  0x44,0x84,0x85,0x45,0x87,0x47,0x46,0x86,0x82,0x42,0x43,0x83,0x41,0x81,0x80,0x40,
 };
 
-unsigned short CRC16b(unsigned char *Buff, unsigned short Count, unsigned char base)
+unsigned short CRC16b( char *Buff, unsigned short Count, unsigned char base)
 {
   unsigned char ah = base;
   unsigned char al = base;
@@ -145,34 +159,54 @@ unsigned short CRC16b(unsigned char *Buff, unsigned short Count, unsigned char b
   return (unsigned short)(al<<8) | ah;
 }
 
-unsigned short CRC16_T(unsigned char *Buff, unsigned short Count)
+unsigned short CRC16_T( char *Buff, unsigned short Count)
 {
   return CRC16b(Buff, Count, 0xFF);
 }
 
-unsigned short  CRC16_T_0(unsigned char *Buff, unsigned short Count)
+unsigned short  CRC16_T_0( char *Buff, unsigned short Count)
 {
   return CRC16b(Buff, Count, 0);
 }
-//-----------------------------
-/*
-unsigned short CRC16_T(unsigned char *Buff, unsigned short Count, unsigned short in_crc)
-{
-  unsigned char al = HIGH(in_crc);
-  unsigned char ah = LOW(in_crc);
-  unsigned char bl;
-  
-  if(Count<3)return (0x000F);
-  while (Count) {
-    bl = *Buff ^ ah;
-    ah = al ^ CRC_Hi[bl];
-    al = CRC_Lo[bl];
-    Buff++; Count--;
-  }
-  return (unsigned short)(al<<8) | ah;
-}
-*/
+
 //-----------------------------standard functions which had been modified-------
+
+char* strncpy_m( char* e_str, char* s_str, int32_t dest_size )
+{
+   char* dst_str = strncpy(e_str, s_str, dest_size);
+
+   if (dest_size > 0)
+   {
+      e_str[dest_size - 1]= '\0';
+   }
+	
+   return dst_str;
+}
+
+void* memset_m( void* ptr, int value, int32_t num, int32_t dest_size )
+{
+   void* res_ptr = 0;
+
+   if(num >= dest_size)
+   {
+      res_ptr = memset(ptr, value, dest_size);
+   }
+   else
+   {
+      res_ptr = memset(ptr, value, num); 
+   }
+   return res_ptr;
+}
+
+uint32_t strlen_m( char*string, uint32_t reasonvalue )
+{
+  uint32_t len = strlen(string);
+  if(len > reasonvalue) len = reasonvalue;
+
+  return(len);
+}
+
+
 
 bool mod_memcpy( char*to, char*from, unsigned short datasize, unsigned short sizeofto )
 {
@@ -182,23 +216,6 @@ bool mod_memcpy( char*to, char*from, unsigned short datasize, unsigned short siz
     memcpy(to, from, sizeofto); 
     return false;
   }
-}
-
-bool mod_memset( char*to, char simb, unsigned short datasize, unsigned short sizeofto )
-{
-  if(sizeofto >= datasize) {memset(to, simb, datasize); return true;}
-  else{
-    //ASSERTION(e_memset);
-    memset(to, simb, sizeofto); 
-    return false;
-  }
-}
-
-unsigned short mod_strlen( char*string, unsigned short reasonvalue )
-{
-  unsigned short len = strlen(string);
-  if(len > reasonvalue) len = reasonvalue;
-  return(len);
 }
 
 bool mod_strstr( char*string1, char*string2, unsigned short len1, unsigned short len2 )
@@ -222,284 +239,11 @@ char* mod_strrchr( char* string1, char symbol, unsigned short sStringSize )
   return tmp_out;
 }
 
-bool mod_strncpy( char* e_str, char* s_str, unsigned short sizeofto )
-{
-  unsigned short n_term = mod_strlen( s_str, sizeofto );
-  return mod_memcpy( e_str, s_str, n_term, sizeofto );
-}
+
 
 bool mod_strcmp( char *s1, char *s2 )    // true - if equil
 {
   bool result = false;
   if( strcmp(s1,s2) == 0 ) result = true;
   return result;
-}
-
-int mod_sprintf_new( char *e_str, unsigned short dest_size, const char* pFormat, va_list *dataList )
-{ 
-  const char *sofs = pFormat;
-  short pointr_in_buf = 0;
-
-  va_list argPtr = *dataList;
-
-  while( (*sofs != 0) && (pointr_in_buf < dest_size) )
-  {       
-       char symbol = *sofs++;
-        
-       if( symbol == '%')
-       {               
-         //content analisis
- 	 symbol = *sofs++;
-
-	 switch( symbol )
-         {
-
-		case  0  : {break;}
-		case 's' : {
-                              char *string_strt = va_arg( argPtr, char * );
-                              
-                              short s_counter = 0;
-                              char data = string_strt[s_counter++];
-
-                              while( (data != 0) &&  (pointr_in_buf <= dest_size) ) 
-                              {                               
-                                e_str[pointr_in_buf++] = data;                             
-                                data = string_strt[s_counter++];  
-                              }   
-                              break;
-                           }                
-		case 'b' : {
-                              char data = va_arg( argPtr, int );
-                              pointr_in_buf += printinbuff( (e_str+pointr_in_buf), data, dest_size-pointr_in_buf );  
-                              break;
-                           }
-		case 'w' : {
-                              short data = va_arg( argPtr, int );
-                              pointr_in_buf += printinbuff( (e_str+pointr_in_buf), data, dest_size-pointr_in_buf ); 
-                              break;
-                           }
-                case 'l' : {
-                              long data = va_arg( argPtr, int );
-                              pointr_in_buf += printinbuff( (e_str+pointr_in_buf), data, dest_size-pointr_in_buf ); 
-                              break;
-                           }
-		case '.' : {
-                              char dig_after_point = (*sofs++)-0x30;
-                              if( (*sofs++ == 'f') && (dig_after_point < 9 ) )
-                              {
-                                float data = va_arg( argPtr, double );
-                                pointr_in_buf += printinbuff( (e_str+pointr_in_buf), data, dig_after_point, dest_size-pointr_in_buf );                               
-                              }
-                              break;
-                           }
-		default	 : {
-                              break;
-                           }
-	 }	
-	}
-        else
-        {
-	  // put symbol in buffer in position pointr_in_buf
-	  e_str[pointr_in_buf++] = symbol;
-	}       
-  }
-
-  return pointr_in_buf;
-}
-
-int mod_sprintf( char *e_str, unsigned short dest_size, char *format, void*ptr1,  
-                                     void*ptr2, void*ptr3, void*ptr4, void*ptr5, void*ptr6 )
-{ 
-  char *sofs = format;
-  short pointr_in_buf = 0;
-  char total_counter = 1;
-  void *ptr_on_paramiter = NULL;
-  
-  while( (*sofs != 0) && (pointr_in_buf < dest_size) )
-  {       
-       char symbol = *sofs++;
-        
-       if( symbol == '%')
-       {
-         
-         // calculation of location data in the stack with the possibility 
-         // of finding elements in the registers !!! only for ARV      
-         switch( total_counter++ ){
-           case  1 : { ptr_on_paramiter = ptr1; break; } 
-           case  2 : { ptr_on_paramiter = ptr2; break; } 
-           case  3 : { ptr_on_paramiter = ptr3; break; } 
-           case  4 : { ptr_on_paramiter = ptr4; break; } 
-           case  5 : { ptr_on_paramiter = ptr5; break; } 
-           case  6 : { ptr_on_paramiter = ptr6; break; }  
-           default : { break; }
-         }  
-         // finished calculation of parameters location 
-                           
-         //content analisis
- 	 symbol = *sofs++;
-
-	 switch( symbol )
-         {
-
-		case  0  : {break;}
-		case 's' : {
-                              char *string_strt = reinterpret_cast<char*>(ptr_on_paramiter);
-                              
-                              short s_counter = 0;
-                              char data = string_strt[s_counter++];
-
-                              while( (data != 0) &&  (pointr_in_buf <= dest_size) ) 
-                              {                               
-                                e_str[pointr_in_buf++] = data;                             
-                                data = string_strt[s_counter++];  
-                              }   
-                              break;
-                           }
-		case 'b' : {
-                              char data = *( reinterpret_cast<char*>(ptr_on_paramiter) );
-                              pointr_in_buf += printinbuff( (e_str+pointr_in_buf), data, dest_size-pointr_in_buf );  
-                              break;
-                           }
-		case 'w' : {
-                              short data = *( reinterpret_cast<short*>(ptr_on_paramiter) );
-                              pointr_in_buf += printinbuff( (e_str+pointr_in_buf), data, dest_size-pointr_in_buf ); 
-                              break;
-                           }
-                case 'l' : {
-                              long data = *( reinterpret_cast<long*>(ptr_on_paramiter) );
-                              pointr_in_buf += printinbuff( (e_str+pointr_in_buf), data, dest_size-pointr_in_buf ); 
-                              break;
-                           }
-		case '.' : {
-                              char dig_after_point = (*sofs++)-0x30;
-                              if( (*sofs++ == 'f') && (dig_after_point < 9 ) )
-                              {
-                                float data = *( reinterpret_cast<float*>(ptr_on_paramiter) );
-                                pointr_in_buf += printinbuff( (e_str+pointr_in_buf), data, dig_after_point, dest_size-pointr_in_buf );                               
-                              }
-                              break;
-                           }
-		
-		default	 : {
-                              break;
-                           }
-	 }	
-	}
-        else
-        {
-	  // put symbol in buffer in position pointr_in_buf
-	  e_str[pointr_in_buf++] = symbol;
-	}
-       
-  }
-  
-  return pointr_in_buf;
-}
-
-//------------------------basic function for print in buffer--------------------
-
-unsigned short printinbuff( char* dest,char* src,unsigned short dest_size )
-{
-  unsigned short str_size = 0; 
-  if(dest && src && dest_size){
-    str_size = (unsigned short)strlen(src);      
-    mod_memcpy(dest,src,str_size,dest_size);               
-  }else{ /*ASSERTION(e_sprnbuf);*/ }
-  return str_size; 
-}
-
-unsigned short printinbuff( char*dest,char value,unsigned short dest_size )
-{
-  unsigned short custom_counter = 0;
-  if(dest && dest_size){
-    do{
-      dest[custom_counter++] = value%10 + 0x30;
-      value /=10;             
-    }while(value && (dest_size > custom_counter));    
-    revertbuff(dest,custom_counter);
-  }else{ /*ASSERTION(e_cprnbuf);*/ } 
-  return custom_counter;
-}
-
-unsigned short printinbuff( char*dest,unsigned short value,unsigned short dest_size )
-{
-  unsigned short custom_counter = 0;
-  if(dest && dest_size){  
-    do{
-      dest[custom_counter++] = value%10 + 0x30;
-      value /=10;             
-    }while(value && (dest_size > custom_counter));    
-    revertbuff(dest,custom_counter);
-  }else{ /*ASSERTION(e_iprnbuf);*/ }  
-  return custom_counter;
-}
-
-unsigned short printinbuff( char*dest,unsigned long value,unsigned short dest_size )
-{
-  unsigned short custom_counter = 0;
-  if(dest && dest_size){  
-    do{
-      dest[custom_counter++] = value%10 + 0x30;
-      value /=10;             
-    }while(value && (dest_size > custom_counter));    
-    revertbuff(dest,custom_counter);
-  }else{ /*ASSERTION(e_lprnbuf);*/ }  
-  return custom_counter;
-}
-
-//------------------------derivative functions for the print in a buffer--------
-
-unsigned short printinbuff( char*dest, short value,unsigned short dest_size )
-{
-  unsigned short custom_counter = 0;
-  if( (value >= 0) && (dest_size > 0) ){
-    custom_counter = printinbuff( dest, (unsigned short)value, dest_size );
-  }else{    
-    value *=-1;
-    dest[custom_counter++] = '-';
-    custom_counter += printinbuff( dest+custom_counter, value, dest_size-custom_counter );
-  }  
-  return custom_counter;
-}
-
-unsigned short printinbuff( char*dest, long value, unsigned short dest_size )
-{
-  unsigned short custom_counter = 0;
-  if( (value >= 0) && (dest_size > 0) ){
-    custom_counter = printinbuff( dest, (unsigned long)value, dest_size );
-  }else{    
-    value *=-1;
-    dest[custom_counter++] = '-';
-    custom_counter += printinbuff( dest+custom_counter, value, dest_size-custom_counter );
-  }  
-  return custom_counter;
-}
-
-unsigned short printinbuff( char*dest,float value, char dig_after_point, unsigned short dest_size )
-{
-  unsigned short custom_counter = 0;
-
-  long main_part = static_cast<long>(value);
-  unsigned long after_point_part = 0;
-  
-  if( main_part >= 0 ){    
-    after_point_part = static_cast<long>( (value - static_cast<float>(main_part)) * pow((double)10, (int)dig_after_point) );
-  }else{  
-    after_point_part = static_cast<long>( (static_cast<float>(main_part) - value) * pow((double)10, (int)dig_after_point) );  
-  }  
-  
-  custom_counter = printinbuff( dest, main_part, dest_size );
-  if( (dig_after_point > 0) && ((dest_size - custom_counter) > 0) )
-  {
-    dest[custom_counter++] = '.';
-    custom_counter += printinbuff( dest+custom_counter, after_point_part, dest_size - custom_counter );
-  }
-  
-  return custom_counter;
-}
-
-unsigned short checkdest( unsigned short &dest_size, unsigned short &busy_size )
-{
-    if(dest_size > busy_size)return(dest_size-busy_size); 
-    return 0;
 }
