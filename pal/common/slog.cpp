1eb4773eb41eae6ece8a34fd7c0f442cdbc55770
AZhigaylo <antoooon@gmail.com>
2014-12-04 11:07:54 +0200

    work

--- pal/common/slog.cpp
+++ pal/common/slog.cpp
@@ -10,13 +10,10 @@ void printError(const char * const fmt,...)
 {
    va_list vargs;
    setlogmask (LOG_UPTO (LOG_ERR));
-   openlog ("home-brain:", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
 
    va_start( vargs, fmt );
    vsyslog( LOG_ERR, fmt, vargs );
    va_end( vargs );
-
-   closelog ();
 }
 
 void printWarning(const char * const fmt, ...)
@@ -25,13 +22,10 @@ void printWarning(const char * const fmt, ...)
    {
       va_list vargs;
       setlogmask (LOG_UPTO (LOG_WARNING));
-      openlog ("home-brain:", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
 
       va_start( vargs, fmt );
       vsyslog( LOG_WARNING, fmt, vargs );
       va_end( vargs );
-
-      closelog ();
    }
 }
 
@@ -41,12 +35,9 @@ void printDebug(const char * const fmt, ...)
    {
       va_list vargs;
       setlogmask (LOG_UPTO (LOG_NOTICE));
-      openlog ("home-brain:", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
 
       va_start( vargs, fmt );
       vsyslog( LOG_NOTICE, fmt, vargs );
       va_end( vargs );
-
-      closelog ();
    }
 }
